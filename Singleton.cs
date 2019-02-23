341a4b9272093270d074623d7886a072721a7be6
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2019-02-23 21:41:04 +0200

    Added a base Singleton script for defining any singleton objects.

---
+++ Singleton.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Utilities
+{
+	/// <summary>
+	/// A base class for creating singleton objects.
+	/// </summary>
+	/// <typeparam name="T">The concrete type.</typeparam>
+	public abstract class Singleton<T> : MonoBehaviour where T : Singleton<T>
+	{
+		#region PROPERTIES
+		public static T instance { get; private set; }
+		#endregion
+
+
+		#region UNITY EVENTS
+		protected virtual void Awake()
+		{
+			if (instance == null)
+			{
+				instance = (T)this;
+			}
+			else
+			{
+				Debug.LogError($"An instance of manager {instance.GetType().Name} already exists in scene.");
+			}
+		}
+
+		protected virtual void OnDestroy()
+		{
+			if (instance == this)
+			{
+				instance = null;
+			}
+		}
+		#endregion
+	}
+}
