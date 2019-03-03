81b02aa1d473882397d5de7624e647ad91320c7d
Kevin Lloyd Baynham <kevin.baynham.k7@gmail.com>
2019-03-03 17:56:59 +0200

    Added editor script for LinearState.

---
+++ StateMachine/States/Editor/LinearStateEditor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+using System.Collections.Generic;
+
+using Utilities.StateMachine.States;
+
+namespace Utilities.StateMachine
+{
+	[CustomEditor(typeof(LinearState)), CanEditMultipleObjects]
+	class LinearStateEditor : Editor
+	{
+		#region UNITY EVENTS
+		public override void OnInspectorGUI()
+		{
+			var state = target as LinearState;
+
+			EditorGUI.BeginDisabledGroup(true);
+			EditorGUILayout.ObjectField("Script", MonoScript.FromMonoBehaviour(state), typeof(MonoScript), false);
+			EditorGUI.EndDisabledGroup();
+
+			var excludedProperties = new List<string> {
+				"m_Script"
+			};
+
+			if (state.isTerminal)
+			{
+				excludedProperties.Add("m_NextState");
+			}
+
+			DrawPropertiesExcluding(serializedObject, excludedProperties.ToArray());
+			serializedObject.ApplyModifiedProperties();
+		}
+		#endregion
+	}
+}
