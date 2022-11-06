1d00c152e363a977f1440c74b626ee310b91a0aa
Kevin Baynham <kevin.baynham.k7@gmail.com>
2022-11-06 09:43:22 +0200

    Added an animation event notifier class.
    This can be used to listen to animation events in a generic way.

---
+++ Animations/AnimationEventNotifier.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Utilities.Animations
+{
+	/// <summary>
+	/// A generic way of receiving animation events from <see cref="Animation"/> clips.
+	/// Listeners can then subscribe to this component's event and respond as needed.
+	/// </summary>
+	public class AnimationEventNotifier : MonoBehaviour
+	{
+		#region EVENTS
+		public event Action onEventReceived = delegate { };
+		public event Action<int> onIntEventReceived = delegate { };
+		public event Action<float> onFloatEventReceived = delegate { };
+		public event Action<string> onStringEventReceived = delegate { };
+		#endregion
+
+
+		#region PUBLIC API
+		public void ReceiveEvent()
+		{
+			onEventReceived();
+		}
+
+		public void ReceiveEventWithInt(int i)
+		{
+			onIntEventReceived(i);
+		}
+
+		public void ReceiveEventWithFloat(float f)
+		{
+			onFloatEventReceived(f);
+		}
+
+		public void ReceiveEventWithString(string s)
+		{
+			onStringEventReceived(s);
+		}
+		#endregion
+	}
+}
