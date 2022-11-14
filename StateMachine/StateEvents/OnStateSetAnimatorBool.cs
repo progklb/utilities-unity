3ac2a5ab7bb22add919b0358813b855ec225f883
Kevin Baynham <kevin.baynham.k7@gmail.com>
2022-11-14 07:44:48 +0200

    Added utility to play animations via the state machine.

---
+++ StateMachine/StateEvents/OnStateSetAnimatorBool.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Utilities.StateMachine.StateEvents
+{
+	public class OnStateSetAnimatorBool : OnStateBehaviour
+	{
+		#region VARIABLES
+		[SerializeField]
+		private Animator m_Animator;
+		[SerializeField]
+		private string m_AnimationKey;
+		[SerializeField]
+		private bool m_ValueOnBegin;
+		[SerializeField]
+		private bool m_ValueOnEnd;
+		#endregion
+
+
+		#region PUBLIC API
+		public override void OnBegin(IState previousState, IState currentState)
+		{
+			if (m_Animator.GetBool(m_AnimationKey) != m_ValueOnBegin)
+			{
+				m_Animator.SetBool(m_AnimationKey, m_ValueOnBegin);
+			}
+		}
+
+		public override void OnEnd(IState currentState, IState nextState)
+		{
+			if (m_Animator.GetBool(m_AnimationKey) != m_ValueOnEnd)
+			{
+				m_Animator.SetBool(m_AnimationKey, m_ValueOnEnd);
+			}
+		}
+		#endregion
+	}
+}
